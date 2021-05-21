361b46bb4c4e3d260215f750be9227f76d2ba576
Nazar <nazariykharchuk@gmail.com>
2021-05-21 11:51:57 +0300

    Оформив введення розміру масиву

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QRegularExpression reg_exp("[0-9]{1,5}");
+    ui->sizeEdit->setValidator(new QRegularExpressionValidator(reg_exp, this));
+    connect(ui->sizeEdit, SIGNAL(textEdited(QString)), this, SLOT(SizeEdit_changet()));
+    //connect(ui->createButton, SIGNAL(clicked()), this, SLOT(accept()));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +17,12 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::SizeEdit_changet(){
+    ui->statusbar->clearMessage();
+    ui->sizeEdit->setStyleSheet("background : white;");
+    ui->createButton->setEnabled(!ui->sizeEdit->text().isEmpty() && ui->sizeEdit->text().toInt()<=20000);
+    if(ui->sizeEdit->text().toInt()>20000){
+        ui->statusbar->showMessage("Введіть значення, менше 20000.");
+        ui->sizeEdit->setStyleSheet("background : red;");
+    }
+}
